759cd86d5d712bb1cd0f6c10eabcd1682bcdfea5
jiangzhen <410096427@qq.com>
2015-05-11 10:01:41 +0800

    add demo test

---
+++ UnitTestDemo/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo
+{
+    public class Test
+    {
+        public string PublicWork()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private string PrivateWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CallPrivateWork()
+        {
+            return this.PrivateWork();
+        }
+    }
+}
